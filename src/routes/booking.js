6f6e01311ecd4856a6b5d2feddc344b1f40baa71
Iwaa Kusumah <iwaakusumah@gmail.com>
2023-12-03 19:27:43 +0700

    first commit

--- src/routes/booking.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const booking = require('../controllers/booking');
-
-router.get('/booking', booking.getBooking);
-router.post('/booking', booking.addBooking);
-
-module.exports = router;
--- src/routes/destination.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const destination = require('../controllers/destination');
-
-router.get('/destination', destination.getDestination);
-router.post('/destination', destination.addDestination);
-
-module.exports = router;
