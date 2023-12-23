27a5f09a6489ff71d8b254b1c9e3c79ad7be620f
Iwaa Kusumah <107662442+iwaakusumah@users.noreply.github.com>
2023-12-23 08:39:29 +0700

    Booking Controllers

--- src/controllers/booking.js
+++ src/controllers/booking.js
@@ -1,29 +1,79 @@
-const booking = require('../models/booking');
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars')
+const path = require('path');
+const booking = require ('../models/booking');
 
-exports.getBooking = async (req, res) => {
+const getBooking = async (req, res) => {
   try {
-    const booking = await booking.findAll();
-    res.json(booking);
+    const Booking = await booking.findAll();
+    res.status(200).json(Booking);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Server Error');
+    res.status(500).json({});
   }
 };
 
-exports.addBooking = async (req, res) => {
-  const { bookingId, name, email, telepon, dateAt, totalPrice } = req.body;
+const addBooking = async (req, res) => {
   try {
-    const newBooking = await booking.create({
+    const { bookingId, name, email, telepon, quantity, dateAt, totalPrice, productName } = req.body;
+    const Booking = new booking({
         bookingId,
         name,
         email,
         telepon,
+        quantity,
         dateAt,
         totalPrice,
+        productName,
     });
-    res.json(newBooking);
+
+    const addBooking = await Booking.save();
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+        auth: {
+          user: 'sukatripnews00@gmail.com',
+          pass: 'psrx pbbl uuzo ffsx',
+        },
+      });
+      
+      const handlebarOptions = {
+        viewEngine: {
+            partialsDir: path.resolve('./views/'),
+            defaultLayout: false,
+        },
+        viewPath: path.resolve('./views/'),
+    };
+    transporter.use('compile', hbs(handlebarOptions));
+      // Setup email data
+      const formattedTotalPrice = totalPrice.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' });
+      const mailOptions = {
+        from: 'sukatripnews00@gmail.com',
+        to: email,
+        subject: 'Booking Ticket Suka Trip Asia',
+        template: 'email',
+        context: {
+          name,
+          email,
+          telepon,
+          quantity,
+          dateAt,
+          formattedTotalPrice,
+          productName,
+        },
+      };
+      
+      // Send the email
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.error('Error:', error);
+        }
+        console.log('Email sent:', info.response);
+      });
+    res.status(201).json({ message: 'Booking created successfully', data: addBooking });
   } catch (error) {
     console.error(error);
-    res.status(500).send('Server Error');
+    res.status(500).json({ message: 'Failed to add booking' });
   }
 };
+
+module.exports = { addBooking, getBooking };
